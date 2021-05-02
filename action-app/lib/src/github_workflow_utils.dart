44b55c207a9db9f0e4288747bba565e96bcfba67
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-05-02 22:14:16 +0300

    Introduce currentCommitSHA (#3)
    * build: improve build script
    
    * feat: add currentCommitSHA
    
    * chore: add stub main

---
+++ action-app/lib/src/github_workflow_utils.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'dart:io';
+
+/// Returns head SHA of the commit associated to the current workflow
+String? currentCommitSHA() {
+  final commitSha = Platform.environment['GITHUB_SHA'];
+  stderr.writeln('SHA that triggered the workflow: $commitSha');
+
+  final pathEventPayload = Platform.environment['GITHUB_EVENT_PATH'];
+  if (pathEventPayload != null) {
+    final eventPayload = jsonDecode(File(pathEventPayload).readAsStringSync())
+        as Map<String, dynamic>;
+
+    final pullRequest = eventPayload['pull_request'] as Map<String, dynamic>?;
+    if (pullRequest != null) {
+      final baseSha =
+          (pullRequest['base'] as Map<String, dynamic>)['sha'] as String;
+      final headSha =
+          (pullRequest['head'] as Map<String, dynamic>)['sha'] as String;
+      if (commitSha != headSha) {
+        stderr..writeln('Base SHA: $baseSha')..writeln('Head SHA: $headSha');
+
+        return headSha;
+      }
+    }
+  }
+
+  return commitSha;
+}
