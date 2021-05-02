13b609b34ded5fb34b1bfa47861672014c4153b6
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-05-03 00:20:54 +0300

    feat: introduce GitHub Action input (#5)
    * feat: introduce GitHub Action input

---
+++ action-app/lib/src/github_action_input.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+/// Objects that represent an input of the action
+class ActionInput {
+  /// The name of the input, as written in the YAML file.
+  final String name;
+
+  final bool isRequired;
+  final bool canBeEmpty;
+
+  const ActionInput(
+    this.name, {
+    required this.isRequired,
+    this.canBeEmpty = true,
+  });
+
+  /// Will throw an [ArgumentError]
+  /// if the input is required and the value is null
+  /// or if the value is an empty string and [canBeEmpty] is false.
+  String get value {
+    final key = 'INPUT_${name.toUpperCase().replaceAll(" ", "_")}';
+    final value = Platform.environment[key];
+    if ((value == null && isRequired) ||
+        (value != null && value.isEmpty && !canBeEmpty)) {
+      throw ArgumentError("No value was given for the argument '$name'.");
+    }
+
+    return value ?? '';
+  }
+}
