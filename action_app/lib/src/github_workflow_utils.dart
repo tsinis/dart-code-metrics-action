f7e505ba1622d1fe20dabc67ca93e5400dff0dd6
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-05-03 08:16:08 +0300

    GitHub Workflow log debug message (#11)
    * feat: introduce GitHub Workflow log debug message
    
    * refactor: migrate on logDebugMessage from stderr.writeln

--- action_app/lib/src/github_workflow_utils.dart
+++ action_app/lib/src/github_workflow_utils.dart
@@ -6,7 +6,7 @@ const _envVarGitHubWorkspace = 'GITHUB_WORKSPACE';
 /// Returns head SHA of the commit associated to the current workflow
 String currentCommitSHA() {
   final commitSha = Platform.environment['GITHUB_SHA'] as String;
-  stderr.writeln('SHA that triggered the workflow: $commitSha');
+  logDebugMessage('SHA that triggered the workflow: $commitSha');
 
   final pathEventPayload = Platform.environment['GITHUB_EVENT_PATH'];
   if (pathEventPayload != null) {
@@ -20,7 +20,8 @@ String currentCommitSHA() {
       final headSha =
           (pullRequest['head'] as Map<String, dynamic>)['sha'] as String;
       if (commitSha != headSha) {
-        stderr..writeln('Base SHA: $baseSha')..writeln('Head SHA: $headSha');
+        logDebugMessage('Base SHA: $baseSha');
+        logDebugMessage('Head SHA: $headSha');
 
         return headSha;
       }
@@ -48,6 +49,20 @@ String currentPathToRepoRoot() {
   return repoPath;
 }
 
+/// Prints a debug message to the log.
+///
+/// You must [enabling step debug logging](https://docs.github.com/en/actions/managing-workflow-runs/enabling-debug-logging#enabling-step-debug-logging)
+/// to see the debug messages set by this command in the log.
+/// To learn more about creating secrets and using them in a step,
+/// see "[Creating and using encrypted secrets.](https://help.github.com/en/actions/automating-your-workflow-with-github-actions/creating-and-using-encrypted-secrets)"
+void logDebugMessage(
+  String message, {
+  String? file,
+  String? line,
+  String? column,
+}) =>
+    _log('debug', message, file, line, column);
+
 void _echo(String command, {String? message, Map<String, String>? parameters}) {
   final sb = StringBuffer('::$command');
 
@@ -71,5 +86,12 @@ void _log(
   String? file,
   String? line,
   String? column,
-) =>
-    _echo(command, message: message, parameters: _params(file, line, column));
+) {
+  final parameters = {
+    if (file != null) 'file': file,
+    if (line != null) 'line': line,
+    if (column != null) 'col': column,
+  };
+
+  _echo(command, message: message, parameters: parameters);
+}
